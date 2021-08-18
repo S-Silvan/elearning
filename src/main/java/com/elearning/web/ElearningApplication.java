2936527f0c0c1afa34313b81de2e24759ea05282
S-Silvan <silvan8124@gmail.com>
2021-08-18 20:38:48 +0530

    Project structure change

--- src/main/java/com/elearning/web/ElearningApplication.java
+++ src/main/java/com/elearning/web/ElearningApplication.java
@@ -1,4 +1,4 @@
-package com.elearning.web.elearning;
+package com.elearning.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
