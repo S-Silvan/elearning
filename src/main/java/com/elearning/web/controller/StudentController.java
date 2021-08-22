f8fc4dabfef5c4114b3bf182a2504f6f4dea9a61
Kamesh V R <67401750+kamesh15@users.noreply.github.com>
2021-08-23 01:03:52 +0530

    added service, controller

--- src/main/java/com/elearning/web/controller/StudentController.java
+++ src/main/java/com/elearning/web/controller/StudentController.java
@@ -1,5 +1,44 @@
 package com.elearning.web.controller;
 
-public class StudentController {
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.elearning.web.model.Assignment;
+import com.elearning.web.service.StudentService;
+
+@RestController
+public class StudentController {
+	private StudentService studentService;
+	
+	@PostMapping("/add-assignment")
+	@ResponseBody
+	public Assignment addAssignment(@RequestBody Assignment assignment) {
+		studentService.addAssignment(assignment);
+		return assignment;
+	}
+	
+	@GetMapping("/get-assignment/{assignment_id}")
+	@ResponseBody
+	public Assignment getOneAssignment(@PathVariable("assignment_id")Integer assignment_id) {
+		Assignment assignment=studentService.getOneAssignment(assignment_id);
+		return assignment;
+	}
+	
+	@PutMapping(path="/update-assignment")
+	public Assignment updateAssignment(@RequestBody Assignment assignment) {
+		studentService.updateAssignment(assignment);
+		return assignment;
+	}
+	
+	@DeleteMapping("/delete-assignment/{assignment_id}")
+	public Assignment deleteAssignment(@PathVariable("assignment_id")Integer assignment_id) {
+		return studentService.deleteAssignment(assignment_id);
+	}
 }
+
---
+++ src/main/java/com/elearning/web/service/StudentServiceImplementation.java
@@ -0,0 +1,40 @@
+package com.elearning.web.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.elearning.web.dao.ProfileDao;
+import com.elearning.web.dao.StudentDao;
+import com.elearning.web.model.Assignment;
+import com.elearning.web.model.Student;
+
+@Component
+public class StudentServiceImplementation implements StudentService {
+	@Autowired
+	private StudentDao studentDao;
+
+	@Override
+	public Assignment addAssignment(Assignment assignment) {
+		return studentDao.save(assignment);
+	}
+
+	@Override
+	public Assignment getOneAssignment(Integer assignment_id) {
+		Optional<Assignment> assignment=studentDao.findById(assignment_id);
+		return assignment.isPresent()?(Assignment)assignment.get():null;
+	}
+
+	@Override
+	public Assignment updateAssignment(Assignment assignment) {
+		return studentDao.save(assignment);
+	}
+
+	@Override
+	public Assignment deleteAssignment(Integer assignment_id){
+		Assignment assignment=getOneAssignment(assignment_id);
+		studentDao.deleteById(assignment_id);
+		return assignment;
+	}
+}
