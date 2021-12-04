6ed32209f9059d13ef1272e0cf5e3a0bea013004
Kamesh V R <67401750+kamesh15@users.noreply.github.com>
2021-12-04 20:08:05 +0530

    updated controller

--- src/main/java/com/elearning/web/controller/AdminController.java
+++ src/main/java/com/elearning/web/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.elearning.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import com.elearning.web.model.Teacher;
 import com.elearning.web.service.AdminService;
 
 @RestController
+@Controller
 public class AdminController {
 	private AdminService adminservice;
 	
